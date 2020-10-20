b6f5db880e741f6d4b5a555090f2509a1f0876de
Kazu Yamamoto <kazu@iij.ad.jp>
2020-10-20 11:13:19 +0900

    using wildcards.

--- qif-check.sh
+++ qif-check.sh
@@ -1,7 +1,7 @@
 # /usr/bin/sh
 
 cmd=util/dist/build/qif/qif
-for i in `cat ~/list`; do
+for i in qifs/encoded/qpack-06/*/*.4096.* ; do
   qif=qifs/qifs/`basename $i | sed 's/\..*$//'`.qif
   echo $cmd $i $qif
   $cmd $i $qif
