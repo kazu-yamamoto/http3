fc0ae182959cc27470069240d8ba6764a6146512
Kazu Yamamoto <kazu@iij.ad.jp>
2020-10-26 13:43:42 +0900

    qif-check.sh takes size argument.

--- qif-check.sh
+++ qif-check.sh
@@ -1,8 +1,10 @@
 # /usr/bin/sh
 
+size=$1
+
 cmd=util/dist/build/qif/qif
-for i in qifs/encoded/qpack-06/*/*.4096.* ; do
+for i in qifs/encoded/qpack-06/*/*.out.$size.*.? ; do
   qif=qifs/qifs/`basename $i | sed 's/\..*$//'`.qif
-  echo $cmd $i $qif
-  $cmd $i $qif
+  echo $cmd $size $i $qif
+  $cmd $size $i $qif
 done
