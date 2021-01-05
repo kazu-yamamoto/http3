8b064398fbbbc6e9de4ba1923068989e0a0e3ce4
Kazu Yamamoto <kazu@iij.ad.jp>
2021-01-05 15:06:17 +0900

    using pattern synonyms

--- Network/HTTP3/Error.hs
+++ Network/HTTP3/Error.hs
@@ -1,67 +1,62 @@
+{-# LANGUAGE PatternSynonyms #-}
+
 module Network.HTTP3.Error where
 
-data HTTP3Error = H3NoError
-                | H3GeneralProtocolError
-                | H3InternalError
-                | H3StreamCreationError
-                | H3ClosedCriticalStream
-                | H3FrameUnexpected
-                | H3FrameError
-                | H3ExcessiveLoad
-                | H3IdError
-                | H3SettingsError
-                | H3MissingSettings
-                | H3RequestRejected
-                | H3RequestCancelled
-                | H3RequestIncomplete
-                | H3ConnectError
-                | H3VersionFallback
-                | QpackDecompressionFailed
-                | QpackEncoderStreamError
-                | QpackDecoderStreamError
-                | H3UnknownError Int
-                deriving (Eq, Show)
+newtype HTTP3Error = HTTP3Error Int deriving (Eq, Show)
 
-fromHTTP3Error :: HTTP3Error -> Int
-fromHTTP3Error H3NoError                = 0x100
-fromHTTP3Error H3GeneralProtocolError   = 0x101
-fromHTTP3Error H3InternalError          = 0x102
-fromHTTP3Error H3StreamCreationError    = 0x103
-fromHTTP3Error H3ClosedCriticalStream   = 0x104
-fromHTTP3Error H3FrameUnexpected        = 0x105
-fromHTTP3Error H3FrameError             = 0x106
-fromHTTP3Error H3ExcessiveLoad          = 0x107
-fromHTTP3Error H3IdError                = 0x108
-fromHTTP3Error H3SettingsError          = 0x109
-fromHTTP3Error H3MissingSettings        = 0x10A
-fromHTTP3Error H3RequestRejected        = 0x10B
-fromHTTP3Error H3RequestCancelled       = 0x10C
-fromHTTP3Error H3RequestIncomplete      = 0x10D
-fromHTTP3Error H3ConnectError           = 0x10F
-fromHTTP3Error H3VersionFallback        = 0x110
-fromHTTP3Error QpackDecompressionFailed = 0x200
-fromHTTP3Error QpackEncoderStreamError  = 0x201
-fromHTTP3Error QpackDecoderStreamError  = 0x202
-fromHTTP3Error (H3UnknownError e)       = e
+pattern H3NoError               :: HTTP3Error
+pattern H3NoError                = HTTP3Error 0x100
 
-toHTTP3Error :: Int -> HTTP3Error
-toHTTP3Error 0x100 = H3NoError
-toHTTP3Error 0x101 = H3GeneralProtocolError
-toHTTP3Error 0x102 = H3InternalError
-toHTTP3Error 0x103 = H3StreamCreationError
-toHTTP3Error 0x104 = H3ClosedCriticalStream
-toHTTP3Error 0x105 = H3FrameUnexpected
-toHTTP3Error 0x106 = H3FrameError
-toHTTP3Error 0x107 = H3ExcessiveLoad
-toHTTP3Error 0x108 = H3IdError
-toHTTP3Error 0x109 = H3SettingsError
-toHTTP3Error 0x10A = H3MissingSettings
-toHTTP3Error 0x10B = H3RequestRejected
-toHTTP3Error 0x10C = H3RequestCancelled
-toHTTP3Error 0x10D = H3RequestIncomplete
-toHTTP3Error 0x10F = H3ConnectError
-toHTTP3Error 0x110 = H3VersionFallback
-toHTTP3Error 0x200 = QpackDecompressionFailed
-toHTTP3Error 0x201 = QpackEncoderStreamError
-toHTTP3Error 0x202 = QpackDecoderStreamError
-toHTTP3Error     e = H3UnknownError e
+pattern H3GeneralProtocolError  :: HTTP3Error
+pattern H3GeneralProtocolError   = HTTP3Error 0x101
+
+pattern H3InternalError         :: HTTP3Error
+pattern H3InternalError          = HTTP3Error 0x102
+
+pattern H3StreamCreationError   :: HTTP3Error
+pattern H3StreamCreationError    = HTTP3Error 0x103
+
+pattern H3ClosedCriticalStream  :: HTTP3Error
+pattern H3ClosedCriticalStream   = HTTP3Error 0x104
+
+pattern H3FrameUnexpected       :: HTTP3Error
+pattern H3FrameUnexpected        = HTTP3Error 0x105
+
+pattern H3FrameError            :: HTTP3Error
+pattern H3FrameError             = HTTP3Error 0x106
+
+pattern H3ExcessiveLoad         :: HTTP3Error
+pattern H3ExcessiveLoad          = HTTP3Error 0x107
+
+pattern H3IdError               :: HTTP3Error
+pattern H3IdError                = HTTP3Error 0x108
+
+pattern H3SettingsError         :: HTTP3Error
+pattern H3SettingsError          = HTTP3Error 0x109
+
+pattern H3MissingSettings       :: HTTP3Error
+pattern H3MissingSettings        = HTTP3Error 0x10A
+
+pattern H3RequestRejected       :: HTTP3Error
+pattern H3RequestRejected        = HTTP3Error 0x10B
+
+pattern H3RequestCancelled      :: HTTP3Error
+pattern H3RequestCancelled       = HTTP3Error 0x10C
+
+pattern H3RequestIncomplete     :: HTTP3Error
+pattern H3RequestIncomplete      = HTTP3Error 0x10D
+
+pattern H3ConnectError          :: HTTP3Error
+pattern H3ConnectError           = HTTP3Error 0x10F
+
+pattern H3VersionFallback       :: HTTP3Error
+pattern H3VersionFallback        = HTTP3Error 0x110
+
+pattern QpackDecompressionFailed :: HTTP3Error
+pattern QpackDecompressionFailed = HTTP3Error 0x200
+
+pattern QpackEncoderStreamError :: HTTP3Error
+pattern QpackEncoderStreamError  = HTTP3Error 0x201
+
+pattern QpackDecoderStreamError :: HTTP3Error
+pattern QpackDecoderStreamError  = HTTP3Error 0x202
