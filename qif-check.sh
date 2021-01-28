89dafe4a24ab06deaeeaaee81b3c299df24dc926
Kazu Yamamoto <kazu@iij.ad.jp>
2021-01-28 14:24:31 +0900

    fixing qif-check.

--- qif-check.sh
+++ qif-check.sh
@@ -2,7 +2,8 @@
 
 size=$1
 
-cmd=util/dist/build/qif/qif
+cmd=dist/build/qif/qif
+
 for i in qifs/encoded/qpack-06/*/*.out.$size.*.? ; do
   qif=qifs/qifs/`basename $i | sed 's/\..*$//'`.qif
   echo $cmd $size $i $qif
