21a732a35c563968154c3aab30bb5dcb344ebe3f
Kazu Yamamoto <kazu@iij.ad.jp>
2021-02-01 15:17:28 +0900

    syncing client with quic lib.

--- util/client.hs
+++ util/client.hs
@@ -15,9 +15,7 @@ import Data.IORef
 import Data.UnixTime
 import Foreign.C.Types
 import Network.HTTP.Types
-import qualified Network.HQ.Client as HQ
-import qualified Network.HTTP3.Client as H3
-import qualified Network.QUIC as QUIC
+import Network.QUIC
 import Network.TLS.QUIC
 import System.Console.GetOpt
 import System.Environment
@@ -25,6 +23,9 @@ import System.Exit
 import Text.Printf
 
 import Common
+
+import qualified Network.HQ.Client as HQ
+import qualified Network.HTTP3.Client as H3
 
 data Options = Options {
     optDebugLog   :: Bool
@@ -40,7 +41,7 @@ data Options = Options {
   , optRetry      :: Bool
   , optQuantum    :: Bool
   , optThroughput :: Bool
-  , optMigration  :: Maybe QUIC.Migration
+  , optMigration  :: Maybe Migration
   , optPacketSize :: Maybe Int
   } deriving Show
 
@@ -108,16 +109,16 @@ options = [
     (NoArg (\o -> o { optQuantum = True }))
     "try sending large Initials"
   , Option ['M'] ["change-server-cid"]
-    (NoArg (\o -> o { optMigration = Just QUIC.ChangeServerCID }))
+    (NoArg (\o -> o { optMigration = Just ChangeServerCID }))
     "use a new server CID"
   , Option ['N'] ["change-client-cid"]
-    (NoArg (\o -> o { optMigration = Just QUIC.ChangeClientCID }))
+    (NoArg (\o -> o { optMigration = Just ChangeClientCID }))
     "use a new client CID"
   , Option ['B'] ["nat-rebinding"]
-    (NoArg (\o -> o { optMigration = Just QUIC.NATRebinding }))
+    (NoArg (\o -> o { optMigration = Just NATRebinding }))
     "use a new local port"
   , Option ['A'] ["address-mobility"]
-    (NoArg (\o -> o { optMigration = Just QUIC.MigrateTo }))
+    (NoArg (\o -> o { optMigration = Just MigrateTo }))
     "use a new address and a new server CID"
   , Option ['T'] ["throughput"]
     (NoArg (\o -> o { optThroughput = True }))
@@ -144,7 +145,7 @@ data Aux = Aux {
   , auxCheckClose :: IO Bool
   }
 
-type Cli = Aux -> QUIC.Connection -> IO QUIC.ConnectionStats
+type Cli = Aux -> Connection -> IO ConnectionStats
 
 main :: IO ()
 main = do
@@ -157,33 +158,33 @@ main = do
     let path | ipslen == 3 = ips !! 2
              | otherwise   = "/"
         addr:port:_ = ips
-        conf = QUIC.defaultClientConfig {
-            QUIC.ccServerName = addr
-          , QUIC.ccPortName   = port
-          , QUIC.ccALPN       = \ver -> let (h3X, hqX) = makeProtos ver
-                                            protos
-                                              | optHQ     = [hqX,h3X]
-                                              | otherwise = [h3X,hqX]
-                                        in return $ Just protos
-          , QUIC.ccValidate   = optValidate
-          , QUIC.ccPacketSize = optPacketSize
-          , QUIC.ccDebugLog   = optDebugLog
-          , QUIC.ccConfig     = QUIC.defaultConfig {
-                QUIC.confVersions   = if optVerNego then
-                                        QUIC.GreasingVersion : QUIC.confVersions QUIC.defaultConfig
-                                      else
-                                        QUIC.confVersions QUIC.defaultConfig
-              , QUIC.confParameters = if optQuantum then
-                                        QUIC.defaultParameters {
-                                            QUIC.greaseParameter = Just (BS.pack (replicate 1200 0))
-                                        }
+        conf = defaultClientConfig {
+            ccServerName = addr
+          , ccPortName   = port
+          , ccALPN       = \ver -> let (h3X, hqX) = makeProtos ver
+                                       protos
+                                         | optHQ     = [hqX,h3X]
+                                         | otherwise = [h3X,hqX]
+                                   in return $ Just protos
+          , ccValidate   = optValidate
+          , ccPacketSize = optPacketSize
+          , ccDebugLog   = optDebugLog
+          , ccConfig     = defaultConfig {
+                confVersions   = if optVerNego then
+                                   GreasingVersion : confVersions defaultConfig
                                  else
-                                   QUIC.defaultParameters
-              , QUIC.confKeyLog     = getLogger optKeyLogFile
-              , QUIC.confGroups     = getGroups optGroups
-              , QUIC.confQLog       = optQLogDir
-              , QUIC.confHooks      = QUIC.defaultHooks {
-                    QUIC.onCloseReceived = writeIORef cref True
+                                   confVersions defaultConfig
+              , confParameters = if optQuantum then
+                                   defaultParameters {
+                                       greaseParameter = Just (BS.pack (replicate 1200 0))
+                                     }
+                                 else
+                                   defaultParameters
+              , confKeyLog     = getLogger optKeyLogFile
+              , confGroups     = getGroups optGroups
+              , confQLog       = optQLogDir
+              , confHooks      = defaultHooks {
+                    onCloseReceived = writeIORef cref True
                   }
               }
           }
@@ -200,26 +201,26 @@ main = do
           }
     runClient conf opts aux
 
-runClient :: QUIC.ClientConfig -> Options -> Aux -> IO ()
+runClient :: ClientConfig -> Options -> Aux -> IO ()
 runClient conf opts@Options{..} aux@Aux{..} = do
     auxDebug "------------------------"
-    (info1,info2,res,mig,client') <- QUIC.runQUICClient conf $ \conn -> do
-        i1 <- QUIC.getConnectionInfo conn
-        let client = case QUIC.alpn i1 of
+    (info1,info2,res,mig,client') <- runQUICClient conf $ \conn -> do
+        i1 <- getConnectionInfo conn
+        let client = case alpn i1 of
               Just proto | "hq" `BS.isPrefixOf` proto -> clientHQ
               _                                       -> clientH3
         m <- case optMigration of
           Nothing   -> return False
           Just mtyp -> do
-              x <- QUIC.migrate conn mtyp
+              x <- migrate conn mtyp
               auxDebug $ "Migration by " ++ show mtyp
               return x
         t1 <- getUnixTime
         stats <- client aux conn
         print stats
         t2 <- getUnixTime
-        i2 <- QUIC.getConnectionInfo conn
-        r <- QUIC.getResumptionInfo conn
+        i2 <- getConnectionInfo conn
+        r <- getResumptionInfo conn
         printThroughput t1 t2 stats
         return (i1, i2, r, m, client)
     if optVerNego then do
@@ -229,9 +230,9 @@ runClient conf opts@Options{..} aux@Aux{..} = do
         putStrLn "Result: (Q) quantum ... OK"
         exitSuccess
       else if optResumption then do
-        if QUIC.isResumptionPossible res then do
+        if isResumptionPossible res then do
             info3 <- runClient2 conf opts aux res client'
-            if QUIC.handshakeMode info3 == PreSharedKey then do
+            if handshakeMode info3 == PreSharedKey then do
                 putStrLn "Result: (R) TLS resumption ... OK"
                 exitSuccess
               else do
@@ -241,9 +242,9 @@ runClient conf opts@Options{..} aux@Aux{..} = do
             putStrLn "Result: (R) TLS resumption ... NG"
             exitFailure
       else if opt0RTT then do
-        if QUIC.is0RTTPossible res then do
+        if is0RTTPossible res then do
             info3 <- runClient2 conf opts aux res client'
-            if QUIC.handshakeMode info3 == RTT0 then do
+            if handshakeMode info3 == RTT0 then do
                 putStrLn "Result: (Z) 0-RTT ... OK"
                 exitSuccess
               else do
@@ -253,34 +254,34 @@ runClient conf opts@Options{..} aux@Aux{..} = do
             putStrLn "Result: (Z) 0-RTT ... NG"
             exitFailure
       else if optRetry then do
-        if QUIC.retry info1 then do
+        if retry info1 then do
             putStrLn "Result: (S) retry ... OK"
             exitSuccess
           else do
             putStrLn "Result: (S) retry ... NG"
             exitFailure
       else case optMigration of
-             Just QUIC.ChangeServerCID -> do
-                 let changed = QUIC.remoteCID info1 /= QUIC.remoteCID info2
-                 if mig && QUIC.remoteCID info1 /= QUIC.remoteCID info2 then do
+             Just ChangeServerCID -> do
+                 let changed = remoteCID info1 /= remoteCID info2
+                 if mig && remoteCID info1 /= remoteCID info2 then do
                      putStrLn "Result: (M) change server CID ... OK"
                      exitSuccess
                    else do
                      putStrLn $ "Result: (M) change server CID ... NG " ++ show (mig,changed)
                      exitFailure
-             Just QUIC.ChangeClientCID -> do
-                 let changed = QUIC.localCID info1 /= QUIC.localCID info2
+             Just ChangeClientCID -> do
+                 let changed = localCID info1 /= localCID info2
                  if mig && changed then do
                      putStrLn "Result: (N) change client CID ... OK"
                      exitSuccess
                    else do
                      putStrLn $ "Result: (N) change client CID ... NG " ++ show (mig,changed)
                      exitFailure
-             Just QUIC.NATRebinding -> do
+             Just NATRebinding -> do
                  putStrLn "Result: (B) NAT rebinding ... OK"
                  exitSuccess
-             Just QUIC.MigrateTo -> do
-                 let changed = QUIC.remoteCID info1 /= QUIC.remoteCID info2
+             Just MigrateTo -> do
+                 let changed = remoteCID info1 /= remoteCID info2
                  if mig && changed then do
                      putStrLn "Result: (A) address mobility ... OK"
                      exitSuccess
@@ -292,25 +293,25 @@ runClient conf opts@Options{..} aux@Aux{..} = do
                  putStrLn "Result: (D) stream data ... OK"
                  closeReceived <- auxCheckClose
                  when closeReceived $ putStrLn "Result: (C) close received ... OK"
-                 case QUIC.alpn info1 of
+                 case alpn info1 of
                    Nothing   -> return ()
                    Just alpn -> when ("h3" `BS.isPrefixOf` alpn) $
                      putStrLn "Result: (3) H3 transaction ... OK"
                  exitSuccess
 
-runClient2 :: QUIC.ClientConfig -> Options -> Aux -> QUIC.ResumptionInfo -> Cli
-           -> IO QUIC.ConnectionInfo
+runClient2 :: ClientConfig -> Options -> Aux -> ResumptionInfo -> Cli
+           -> IO ConnectionInfo
 runClient2 conf Options{..} aux@Aux{..} res client = do
     threadDelay 100000
     auxDebug "<<<< next connection >>>>"
     auxDebug "------------------------"
-    QUIC.runQUICClient conf' $ \conn -> do
+    runQUICClient conf' $ \conn -> do
         void $ client aux conn
-        QUIC.getConnectionInfo conn
+        getConnectionInfo conn
   where
     conf' = conf {
-        QUIC.ccResumption = res
-      , QUIC.ccUse0RTT    = opt0RTT && QUIC.is0RTTPossible res
+        ccResumption = res
+      , ccUse0RTT    = opt0RTT && is0RTTPossible res
       }
 
 clientHQ :: Cli
@@ -319,7 +320,7 @@ clientHQ = clientX HQ.run
 clientH3 :: Cli
 clientH3 = clientX H3.run
 
-clientX ::  (QUIC.Connection -> H3.ClientConfig -> H3.Config -> H3.Client QUIC.ConnectionStats -> IO QUIC.ConnectionStats) -> Cli
+clientX ::  (Connection -> H3.ClientConfig -> H3.Config -> H3.Client ConnectionStats -> IO ConnectionStats) -> Cli
 clientX run Aux{..} conn = E.bracket H3.allocSimpleConfig H3.freeSimpleConfig $ \conf -> run conn cliconf conf client
   where
     cliconf = H3.ClientConfig {
@@ -332,15 +333,15 @@ clientX run Aux{..} conn = E.bracket H3.allocSimpleConfig H3.freeSimpleConfig $ 
             auxShow "------------------------"
             loop rsp
             auxShow "------------------------"
-            QUIC.getConnectionStats conn
+            getConnectionStats conn
     loop rsp = do
         x <- H3.getResponseBodyChunk rsp
         when (x /= "") $ do
             auxShow x
             loop rsp
 
-printThroughput :: UnixTime -> UnixTime -> QUIC.ConnectionStats -> IO ()
-printThroughput t1 t2 QUIC.ConnectionStats{..} =
+printThroughput :: UnixTime -> UnixTime -> ConnectionStats -> IO ()
+printThroughput t1 t2 ConnectionStats{..} =
     printf "Throughput %.2f MB/s (%d bytes in %d msecs)\n" bytesPerSeconds rxBytes millisecs
   where
     UnixDiffTime (CTime s) u = t2 `diffUnixTime` t1
