cf2187ed1468ce40ca29170ded20f2a435514b21
Kazu Yamamoto <kazu@iij.ad.jp>
2021-11-02 15:41:15 +0900

    using a proper path

--- qif-check.sh
+++ qif-check.sh
@@ -4,7 +4,7 @@ size=$1
 
 cmd=dist/build/qif/qif
 
-for i in qifs/encoded/qpack-06/*/*.out.$size.*.? ; do
+for i in qifs/encoded/qpack-05/*/*.out.$size.*.? ; do
   qif=qifs/qifs/`basename $i | sed 's/\..*$//'`.qif
   echo $cmd $size $i $qif
   $cmd $size $i $qif
