98a3d6d52b0ceb56c0d96f1f37a1328d6e44bc0e
Kazu Yamamoto <kazu@iij.ad.jp>
2025-05-07 13:49:50 +0900

    distinguishing RequiredInsertCount from InsertionPoint

--- test/QPACK/TableSpec.hs
+++ test/QPACK/TableSpec.hs
@@ -9,13 +9,13 @@ spec :: Spec
 spec = do
     describe "encodeRequiredInsertCount and decodeRequiredInsertCount" $ do
         prop "duality" $ \(Triple m ei di) -> do
-            let ereq = encodeRequiredInsertCount m (InsertionPoint ei)
-                InsertionPoint ei' = decodeRequiredInsertCount m (InsertionPoint di) ereq
+            let ereq = encodeRequiredInsertCount m (RequiredInsertCount ei)
+                RequiredInsertCount ei' = decodeRequiredInsertCount m (InsertionPoint di) ereq
             ei' `shouldBe` ei
     describe "encodeBase and decodeBase" $ do
         prop "duality" $ \(Doubl base reqInsCnt) -> do
-            let (s, delta) = encodeBase (InsertionPoint reqInsCnt) (BasePoint base)
-                BasePoint base' = decodeBase (InsertionPoint reqInsCnt) s delta
+            let (s, delta) = encodeBase (RequiredInsertCount reqInsCnt) (BasePoint base)
+                BasePoint base' = decodeBase (RequiredInsertCount reqInsCnt) s delta
             base' `shouldBe` base
 
 data Doubl = Doubl Int Int deriving (Eq, Show)
