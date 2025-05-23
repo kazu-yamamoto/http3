0b06c63978d02a2dafd9e1fb92aad28c20dc3af8
Kazu Yamamoto <kazu@iij.ad.jp>
2025-05-23 12:25:53 +0900

    improving qif-check.sh

--- qif-check.sh
+++ qif-check.sh
@@ -2,13 +2,12 @@
 
 cmd=dist/build/qif-dec/qif-dec
 
-for size in 0 256 512 4096; do
-  for i in qifs/encoded/qpack-05/*/*.out.$size.*.? ; do
-    qif=qifs/qifs/`basename $i | sed 's/\..*$//'`.qif
-    printf "%s " $i
-    $cmd $size $i $qif
-    if [ $? -ne 0 ]; then
-      echo $cmd $size $i $qif
-    fi
-  done
+for enc in qifs/encoded/qpack-05/*/*.out.*.*.? ; do
+  cap=`basename $enc | sed -e 's/^[-a-zA-Z0-9]*\.out\.\([0-9]*\)\.[0-9]*\.[01]$/\1/g'`
+  qif=qifs/qifs/`basename $enc | sed 's/\..*$//'`.qif
+  printf "%s " $enc # excluding \n
+  $cmd $cap $enc $qif
+  if [ $? -ne 0 ]; then
+    echo $cmd $cap $enc $qif
+  fi
 done
