e829ad99b81831e1e33c56b00747db5037ff964b
Kazu Yamamoto <kazu@iij.ad.jp>
2025-05-23 12:26:13 +0900

    implementing qif-gen

---
+++ qif-gen.sh
@@ -0,0 +1,14 @@
+# /bin/sh
+
+cmd=dist/build/qif-enc/qif-enc
+
+for qif in qifs/qifs/*.qif; do
+  for cap in 0 256 512 4096; do
+    for blk in 0 100; do
+      for ack in 0 1; do
+        echo "$cmd $qif $cap $blk $ack"
+        $cmd $qif $cap $blk $ack
+      done
+    done
+  done
+done
